c6aaaa9829a56dd10807cbbce968b78ef469de03
webafra <webafra@gmail.com>
2026-03-11 05:41:29 +0330

    mod: add wallet type in pay method

--- src/Traits/HandlesPayment.php
+++ src/Traits/HandlesPayment.php
@@ -4,30 +4,59 @@ namespace Webafra\LaravelUserWallet\Traits;
 
 use Illuminate\Support\Facades\DB;
 use Webafra\LaravelUserWallet\Exceptions\InsufficientBalanceException;
+use Webafra\LaravelUserWallet\Exceptions\InvalidDepositException;
+use Webafra\LaravelUserWallet\Exceptions\InvalidValueException;
 
 trait HandlesPayment
 {
+
     /**
      * Pay the order value from the user's wallets.
      *
      * @throws InsufficientBalanceException
+     * @throws InvalidDepositException
      */
-    public function pay(int|float $orderValue, ?string $notes = null): void
+
+    public function pay(int|float $amount, ?string $type = null, ?string $notes = null): void
     {
-        if (! $this->hasSufficientBalance($orderValue)) {
-            throw new InsufficientBalanceException('Insufficient balance to cover the order.');
+        $paymentable = $this->getWalletableTypes();
+
+        if ($type) {
+            if (!$this->isRequestValid($type, $paymentable)) {
+                throw new InvalidDepositException('Invalid payment request.');
+            }
         }
 
-        DB::transaction(function () use ($orderValue, $notes) {
-            $remainingOrderValue = $orderValue;
+
+        if ($amount <= 0) {
+            throw new InvalidValueException;
+        }
+
+
+        if ($type) {
+            $balance = $this->getWalletBalanceByType($type);
+            if ($balance < $amount) {
+                throw new InsufficientBalanceException('Insufficient balance to cover the order.');
+            }
+        } else {
+            if (!$this->hasSufficientBalance($amount)) {
+                throw new InsufficientBalanceException('Insufficient balance to cover the order.');
+            }
+        }
+
+
+        DB::transaction(function () use ($amount, $type, $notes) {
+            $remainingOrderValue = $amount;
 
             /**
              * @var \Illuminate\Support\Collection<TKey, \Webafra\LaravelUserWallet\Models\Wallet>
              */
-            $walletsInOrder = $this->wallets()->whereIn('type', $this->walletsInOrder())->get();
+
+            $walletsInOrder = $this->wallets()->whereIn('type', $type ? [$type] : $this->walletsInOrder())->get();
+
 
             foreach ($walletsInOrder as $wallet) {
-                if (! $wallet || ! $wallet->hasBalance()) {
+                if (!$wallet || !$wallet->hasBalance()) {
                     continue;
                 }
 
@@ -45,4 +74,5 @@ trait HandlesPayment
             }
         });
     }
+
 }
